18ede094f1a0b09a47c06a2e4c245a2c2c166e3e
Alexander Terry <alexterry43@gmail.com>
2017-04-08 13:47:16 -0800

    dont fire edit if content of message does not change

--- cogs/events.py
+++ cogs/events.py
@@ -19,10 +19,13 @@ class Events:
         pass
 
     async def on_message_edit(self, before, after):
-        log_message = ":pencil: {0.name}#{0.discriminator} (`{0.id}`) edited their message in {1.channel.mention}\n**B** {1.clean_content}\n**A** {2.clean_content}"
-        #dont forget to add something for message saving to the DB
-        log_message = log_message.format(before.author,before,after)
-        await self.bot.send_message(await self.get_log_channel(before.server.id, 'message-log'), log_message)
+        if before.content == after.content:
+            pass
+        else:
+            log_message = ":pencil: {0.name}#{0.discriminator} (`{0.id}`) edited their message in {1.channel.mention}\n**B** {1.clean_content}\n**A** {2.clean_content}"
+            #dont forget to add something for message saving to the DB
+            log_message = log_message.format(before.author,before,after)
+            await self.bot.send_message(await self.get_log_channel(before.server.id, 'message-log'), log_message)
 
     async def on_message_delete(self, message):
         log_message = ":wastebasket: {0.name}#{0.discriminator} (`{0.id}`) deleted from channel {1.channel.mention} \n{1.clean_content}".format(message.author, message)
