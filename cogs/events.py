e3d2cfa2363b042147288e463a238fc9b38bc068
Alexander Terry <alexterry43@gmail.com>
2017-04-08 11:20:45 -0800

    add channel_delete/create logging, get_logging_channel now takes a serverID instead of a message

--- cogs/events.py
+++ cogs/events.py
@@ -7,11 +7,11 @@ class Events:
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_log_channel(self, message, channel):
+    async def get_log_channel(self, server_id, channel):
         try:
-            channel = self.bot.get_channel(config.guilds[message.server.id][channel])
-        except KeyError as e:
-            channel = self.bot.get_channel(config.guilds[message.server.id]['server-log'])
+            channel = self.bot.get_channel(config.guilds[server_id][channel])
+        except KeyError:
+            channel = self.bot.get_channel(config.guilds[server_id]['server-log'])
         finally:
             return channel
 
@@ -22,11 +22,19 @@ class Events:
         log_message = ":pencil: {0.name}#{0.discriminator} (`{0.id}`) edited their message in {1.channel.mention}\n**B** {1.content}\n**A** {2.content}"
         log_message = log_message.format(before.author,before,after)
         #dont forget to add something for message saving to the DB
-        await self.bot.send_message((await self.get_log_channel(before, 'message-log')), log_message)
+        await self.bot.send_message(await self.get_log_channel(before.server.id, 'message-log'), log_message)
 
     async def on_message_delete(self, message):
         log_message = ":wastebasket: {0.name}#{0.discriminator} (`{0.id}`) deleted from channel {1.channel.mention} \n{1.content}".format(message.author, message)
-        await self.bot.send_message((await self.get_log_channel(message, 'message-log')), log_message)
+        await self.bot.send_message(await self.get_log_channel(message.server.id, 'message-log'), log_message)
+        
+    async def on_channel_create(self, channel):
+        log_message = ":pen_ballpoint: #{0} was created".format(channel.name)
+        await self.bot.send_message(await self.get_log_channel(channel.server.id, 'server-log'), log_message)
+    
+    async def on_channel_delete(self, channel):
+        log_message = ":wastebasket: #{0} was deleted".format(channel.name)
+        await self.bot.send_message(await self.get_log_channel(channel.server.id, 'server-log'), log_message)
 
 def setup(bot):
     bot.add_cog(Events(bot))
