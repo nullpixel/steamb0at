520581810f1b5da153dd19c1d15719329c471f4b
Jamie Bishop <jamie@nullpixel.uk>
2017-04-10 17:50:20 +0100

    Supposedly add #11; not fixed

--- cogs/events.py
+++ cogs/events.py
@@ -20,15 +20,14 @@ class Events:
             return
         if await self.get_log_channel(before.server.id) is None:
             return
-        
-        log_message = ":pencil: {0.name}#{0.discriminator} (`{0.id}`) edited their message in {1.channel.mention}\n**B** {1.clean_content}\n**A** {2.clean_content}"
+        log_message = ":pencil: {0.name}#{0.discriminator} (`{0.id}`) message in {1.channel.mention}\n**B** {1.clean_content}\n**A** {2.clean_content}"
         log_message = log_message.format(before.author,before,after)
         await self.bot.send_message(await self.get_log_channel(before.server.id), log_message)
 
     async def on_message_delete(self, message):
         if await self.get_log_channel(message.server.id) is None:
             return
-        log_message = ":wastebasket: {0.name}#{0.discriminator} (`{0.id}`) deleted from channel {1.channel.mention} \n{1.clean_content}".format(message.author, message)
+        log_message = ":wastebasket: {0.name}#{0.discriminator} (`{0.id}`) message deleted in {1.channel.mention} \n{1.clean_content}".format(message.author, message)
         await self.bot.send_message(await self.get_log_channel(message.server.id), log_message)
         
     async def on_channel_create(self, channel):
@@ -43,6 +42,20 @@ class Events:
         log_message = ":wastebasket: #{0} was deleted".format(channel.name)
         await self.bot.send_message(await self.get_log_channel(channel.server.id), log_message)
 
+    async def on_member_join(self, member):
+        if await self.get_log_channel(member.server.id) is None:
+            return
+        creation = discord.utils.snowflake_time(member.id)
+        log_message = ":inbox_tray: {0.name}#{0.discriminator} (`{0.id}`) joined the server (Created {1})".format(member, creation)
+        await self.bot.send_message(await self.get_log_channel(member.server.id), log_message)
+
+    async def on_member_remove(self, member):
+        if await self.get_log_channel(member.server.id) is None:
+            return
+        log_message = ":outbox_tray: {0.name}#{0.discriminator} (`{0.id}`) left the server".format(member)
+        await self.bot.send_message(await self.get_log_channel(member.server.id), log_message)
+    
+        
     async def on_command_error(self, exception, ctx):
         if isinstance(exception, commands.CheckFailure):
             return
